2a767269f9fe7a14ab3b1ec39dc625fa149c89f4
nwyawka <nwyawka@gmail.com>
2016-12-18 04:59:56 +0000

    still mote but finished

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,27 @@
-var aloha = 'Aloha Variables';
-console.log(aloha)
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT;
+
+
+//Request handlers
+app.use(express.static('public'));
+app.use(express.static('bower_components'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+    //res.send('Aloha World!')
+    res.render('index', { list: ['first','second','third']});
+});
+
+app.get('/routing', function(req, res){
+    res.send('Aloha Routing!');
+});
+
+app.listen(port, function(err){
+    console.log('server is running on port ' + port);
+});
+
